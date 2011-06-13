9a7d1d06841a12b5e2dc16d1aa686b2f6d05a613
Julius de Bruijn <pimeys@gmail.com>
2011-06-14 00:27:03 +0300

    Add soundcloud id to config, generate feed for user with sorted favorites

--- scfeed.rb
+++ scfeed.rb
@@ -1,11 +1,40 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
+require 'builder'
+require File.expand_path(File.dirname(__FILE__) + '/config/settings')
 
 get '/' do
 	File.read(File.join('public', 'index.html'))
 end
 
-post '/' do
+get '/rss/:user' do	
+
+	resp = Net::HTTP.get(URI.parse("http://api.soundcloud.com/users.json?client_id=#{settings.soundcloud_key}&q=#{params[:user]}&limit=1"))
+	users = JSON.parse(resp)
+	redirect to('/error'), 404 and return if users.empty?
+	user = users.first
+	favorites = JSON.parse(Net::HTTP.get(URI.parse("http://api.soundcloud.com/users/#{user['id']}/favorites.json?client_id=#{settings.soundcloud_key}")))
+
+	@favorites_top_list = favorites.reduce({}) do |acc, favorite|
+		user_name = favorite["user"]["username"]
+		if acc[user_name]
+			acc[user_name] += 1
+		else
+			acc[user_name] = 1
+		end
+		acc
+	end
+
+	builder :rss
 end
 
-get '/:id' do
+get '/error' do
+	"User not found :P"
+end
+
+private
+
+def load_config file
+	yaml = YAML.load_file
 end

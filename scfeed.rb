4bfe6cbd7201c169337512b6d5e3066e643ad974
Julius de Bruijn <pimeys@gmail.com>
2011-06-17 00:38:32 +0300

    Haml RSS template

--- scfeed.rb
+++ scfeed.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'json'
 require 'builder'
+require 'rack/cache'
+require 'haml'
 require File.expand_path(File.dirname(__FILE__) + '/lib/soundcloud')
 require File.expand_path(File.dirname(__FILE__) + '/lib/lastfm')
 
@@ -9,14 +11,17 @@ get '/' do
 end
 
 get '/rss/:user' do	
-	user = SoundCloud.find_user(params[:user])
-	if user
-		@favorites_top_list = SoundCloud.favorites(user["id"])
+	content_type 'application/rss+xml'
+	cache_control :public, :must_revalidate, :max_age => 60
+
+	@user = SoundCloud.find_user(params[:user])
+	if @user
+		@favorites_top_list = SoundCloud.favorites(@user["id"])
 		@favorites_top_list.each do |favorite|
 			favorite[1].merge!({:bio => LastFM.artist_bio(favorite[0])})
 		end
 
-		builder :rss
+		haml(:rss, :format => :xhtml, :escape_html => true, :layout => false)
 	else
 		return 404
 	end
